14bb4e48d2f9ba342f220df56163b011990b8c3a
Guicbdiniz <guicbdeniz@hotmail.com>
2022-03-04 12:56:51 -0300

    build: update builded files

---
+++ lib/coinext/index.d.ts
@@ -0,0 +1,38 @@
+import { IBookOrderResponse, IBookOrderResquest, IDepositInfoResponse, IGetDepositsResponse, IInstrument, IInstrumentIdResponse, ILoginResponse, InstrumentSymbol, IPayload, IServiceName, ISimpleLoginResponse, ISubscribeAccountEventsResponse, ISubscribeAccountEventsResquest, ITransferFundsResponse, ProductName } from "./types";
+declare class Coinext {
+    private omsId;
+    private socket;
+    private products;
+    private instruments;
+    connect: () => Promise<void>;
+    disconnect: () => Promise<void>;
+    getCoins: () => string[];
+    getInstruments: () => IInstrument[];
+    refreshProductsAndInstruments: () => Promise<void>;
+    private static createSha256Signature;
+    private static authPayload;
+    login: (username: string, password: string) => Promise<ISimpleLoginResponse>;
+    loginBySecret: () => Promise<ILoginResponse>;
+    getCurrencyDepositAddress: (accountId: number, productName: ProductName) => Promise<IDepositInfoResponse>;
+    subscribeToAccountEvents: ({ accountId, }: ISubscribeAccountEventsResquest) => Promise<ISubscribeAccountEventsResponse>;
+    getBookOrders: ({ instrumentId, howMany, side }: IBookOrderResquest) => Promise<IBookOrderResponse>;
+    getInstrumentIdBySymbol: (instrumentSymbol: InstrumentSymbol) => Promise<IInstrumentIdResponse>;
+    safeCall(serviceName: IServiceName, payload: IPayload): Promise<{
+        response: IPayload | undefined;
+        errorMessage: string;
+    } | {
+        errorMessage: string;
+        response?: undefined;
+    }>;
+    getTickerHistory: (coin: string, intervalInSeconds: number, fromDate: Date, toDate: Date) => Promise<{
+        history: IPayload | undefined;
+        errorMessage: string;
+    } | {
+        history: never[];
+        errorMessage: string;
+    }>;
+    private instrumentForCoin;
+    getDeposits: (accountId: number) => Promise<IGetDepositsResponse>;
+    transferFunds: (senderAccountId: number, productName: ProductName, receiverUsername: string, amount: number, notes: string) => Promise<ITransferFundsResponse>;
+}
+export { Coinext };
